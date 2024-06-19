c17a7c52ba677998ff50742b4ed711f45d10b8e5
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2024-06-19 07:54:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/InputLink', methods=['POST'])
+def InputLink():
+        url = request.form['URL']
+        return render_template('output.html', data=url)
 
 if __name__ == '__main__':
     app.run(debug=True)
